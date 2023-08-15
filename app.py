0c04c0fdbd494bc26490be05d97996dee8e0ff35
Brady <bradyp4080@gmail.com>
2023-08-15 15:29:59 -0500

    changed the git pages from root to docs and set the flask frozen folder to docs

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__,
             static_folder='static',
             template_folder='templates')
 
+app.config['FREEZER_DESTINATION'] = 'docs'
+
 freezer = Freezer(app)
 
 #home page
