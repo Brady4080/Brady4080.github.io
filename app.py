1542012bc2d634309ea4dc28d894d27d3e60417f
Brady <bradyp4080@gmail.com>
2023-08-19 10:46:15 -0500

    decided how I was going about doing the demos was not correct so I just removed it

--- app.py
+++ app.py
@@ -45,44 +45,6 @@ def projects():
 def page_not_found(e):
     return render_template('404.html'), 404
 
-#########################################################
-# DEMOS                                                 #
-#########################################################
-
-# Fetches code content from a GitHub repository using the GitHub API
-def fetch_code_content(owner, repo, file_path):
-    url = f'https://api.github.com/repos/{owner}/{repo}/contents/{file_path}'
-    headers = {
-        'Authorization': f'token {app.config["GITHUB_ACCESS_TOKEN"]}',
-        'Accept': 'application/vnd.github.v3+json'
-    }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-    content = response.json()['content']
-    decoded_content = base64.b64decode(content).decode('utf-8')
-    return decoded_content
-
-@app.route('/run-code', methods=['POST'])
-def run_code():
-    data = request.json
-    result = []
-
-    for repo_info in github_repos:
-        owner = repo_info['owner']
-        name = repo_info['name']
-
-        try:
-            code_content = fetch_code_content(owner, name, 'path_to_file_in_repo.py')
-            exec_result = {}
-            exec(code_content, globals(), exec_result)
-            output = exec_result.get('output', 'No output')
-        except Exception as e:
-            output = str(e)
-
-        result.append({'owner': owner, 'name': name, 'output': output})
-
-    return jsonify(result)
-
 if __name__ == '__main__':
     freezer.freeze()
     app.run(debug=True)
