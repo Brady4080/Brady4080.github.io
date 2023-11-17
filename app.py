05d80bf3bc75713457c81c9c059d0848c13cb3d6
Brady <bradyp4080@gmail.com>
2023-11-17 10:57:19 -0600

    should have fixed issue

--- app.py
+++ app.py
@@ -26,29 +26,28 @@ def index():
     return render_template('index.html')
 
 # About page
-@app.route('/about') 
+@app.route('/about.html') 
 def about():
     return render_template('about.html')
 
 # Resume page
-@app.route('/resume')  
+@app.route('/resume.html')  
 def resume():
     return render_template('resume.html')
 
 # Projects page
-@app.route('/projects')  
+@app.route('/projects.html')  
 def projects():
     return render_template('projects.html')
 
-# Discord verify domain page
-@app.route('/.well-known/discord')  
-def well_known():
-    return render_template('well-known-discord.html')
+@app.route('/.well-known/discord')
+def discord_verification():
+    return 'dh=af9549bdfedf8a98fe9430cd7b3030a6553712f6'
 
 # 404 page
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404'), 404
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
     freezer.freeze()
